5ae4e9f4147cba89d95c7a6b9c134bc5678604e9
Antonio Guerra <antonioguerrained@gmail.com>
2021-09-06 17:58:36 +0100

    Created Users and Bookings controllers

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,16 @@
+class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(status: 0)
+    @event = Event.find(params[:event_id])
+    @booking.event = @event
+    @booking.user = current_user
+
+    authorize @booking
+
+    if @booking.save
+      redirect_to event_path(@event, booked: true)#, notice: 'Great! You just bought an amazing pair of socks!'
+    else
+      render '/events/show'
+    end
+  end
+end
