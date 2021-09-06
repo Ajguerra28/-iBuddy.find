5e945609f3bbb04ea10cf7b9b82bd12f567c1675
Antonio Guerra <antonioguerrained@gmail.com>
2021-09-06 17:55:08 +0100

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :events, except: :destroy do
+    collection do
+      get :my_events
+    end
+    resources :bookings, only: %i[new create show edit]
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
