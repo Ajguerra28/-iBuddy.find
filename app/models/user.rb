3440394eead594ff3df29974ce53be62a1466f46
Antonio Guerra <antonioguerrained@gmail.com>
2021-09-06 17:17:57 +0100

    Changed migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :review_users, foreign_key: :reviewee_id
-  has_many :review_users, foreign_key: :reviewer_id
+  has_many :review_users, foreign_key: :reviewee_id, class_name: "User"
+  has_many :review_users, foreign_key: :reviewer_id, class_name: "User"
   has_many :events
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/migrate/20210906153913_add_reference_to_review_users.rb
+++ db/migrate/20210906153913_add_reference_to_review_users.rb
@@ -1,5 +1,5 @@
 class AddReferenceToReviewUsers < ActiveRecord::Migration[6.1]
   def change
-    add_reference :review_users, :user, index: true, foreign_key: {to_table: :users}
+    add_reference :review_users, :user, index: true
   end
 end
