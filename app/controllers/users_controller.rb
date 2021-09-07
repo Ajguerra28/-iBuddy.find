09d20e30cf88801480bda1324f13dedd5ff581ef
david luis <davidluis2020@gmail.com>
2021-09-07 11:52:54 +0100

    map in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @events = Sock.where(owner: current_user)
+    authorize @user
+    if @user.latitude.present? && @user.longitude.present?
+        @markers = [
+        {
+            lat: @user.latitude,
+            lng: @user.longitude,
+            info_window: render_to_string(partial: "info_window", locals: { user: @user }),
+            # image_url: helpers.asset_url('logosocks.png')
+        }]
+    end
+  end
 end
