15c2ae58e5f0c03e78692497b7a7e86f89bbf6c5
Antonio Guerra <antonioguerrained@gmail.com>
2021-09-07 17:11:34 +0100

    Testing calendar and changed font family on navbar button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import { initMapbox } from '../plugins/init_mapbox';
+// import { initCalendar } from '../plugins/init_calendar';
 
 Rails.start()
 Turbolinks.start()
@@ -29,4 +30,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  // initCalendar();
 });
