5f4a544ab0348ab016dbe9be1e863d08040ecab2
Mariana Kobayashi <marianadanifk@gmail.com>
2021-09-07 15:41:34 +0100

    trying to fix merge issues

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,25 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_booking, only: %i[create]
+
   def create
     @booking = Booking.new(status: 0)
-    @event = Event.find(params[:event_id])
     @booking.event = @event
-    # @booking.user = current_user
-
-    # authorize @booking
+    @booking.user = current_user
 
     if @booking.save
+      @booking.max_people -= 1
+      @booking.save
       redirect_to event_path(@event, booked: true)#, notice: 'Great! You just bought an amazing pair of socks!'
     else
       render '/events/show'
     end
+  end
+
+  private
+
+  def set_booking
+    @event = Event.find(params[:event_id])
+    authorize @booking
   end
 end
