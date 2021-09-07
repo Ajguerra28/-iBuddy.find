ef9022d24e547ea723c1ea52a2498fae9d9e108d
david luis <davidluis2020@gmail.com>
2021-09-07 15:41:45 +0100

    add price to events, max_people to booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_164351) do
+ActiveRecord::Schema.define(version: 2021_09_07_143121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_164351) do
     t.bigint "booking_days_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "max_people"
     t.index ["booking_days_id"], name: "index_bookings_on_booking_days_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -42,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_164351) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "price"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
