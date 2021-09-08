6c6a5a84924a80d66305b5c7d8445f9730634f7a
david luis <davidluis2020@gmail.com>
2021-09-08 11:51:57 +0100

    Add 20 new events

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,10 @@ Event.destroy_all
 User.destroy_all
 
 
-antonio = User.create(name: 'António Guerra', email: 'antonioguerrained@gmail.com', password: '123123', location: 'Via Futebol Clube do Porto', date_of_birth: Faker::Date.birthday(min_age: 24, max_age: 25), gender: 'male', phone_number: '+351927624961', description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
+antonio = User.create(name: 'António Guerra', email: 'antonioguerrained@gmail.com', password: '123123', location: 'Via Futebol Clube do Porto', date_of_birth: Faker::Date.birthday(min_age: 24, max_age: 25), gender: 'male', phone_number: '+101927624961', description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
 
-david = User.create(name: 'David Luis', email: 'davidluis2020@gmail.com', password: '123123', location: 'Rua Manuel Arriaga', date_of_birth: Faker::Date.birthday(min_age: 26, max_age: 27), gender: 'male', phone_number: '+351912345678', description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
-mariana = User.create(name: 'Mariana Kobayashi', email: 'marianadanifk@gmail.com', password: '123123', location: 'Av. do Uruguai 36', date_of_birth: Faker::Date.birthday(min_age: 23, max_age: 25), gender: 'female', phone_number: '+351937826634', description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
+david = User.create(name: 'David Luis', email: 'davidluis2020@gmail.com', password: '123123', location: 'Rua Manuel Arriaga', date_of_birth: Faker::Date.birthday(min_age: 26, max_age: 27), gender: 'male', phone_number: '+101912345678', description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
+mariana = User.create(name: 'Mariana Kobayashi', email: 'marianadanifk@gmail.com', password: '123123', location: 'Av. do Uruguai 36', date_of_birth: Faker::Date.birthday(min_age: 23, max_age: 25), gender: 'female', phone_number: '+101937826634', description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
 
 puts "Hello António, David and Mariana!"
 
@@ -25,7 +25,7 @@ locations = ["Rua Condo Redondo Nº117","Rua Manuel Arriaga Nº8","R. Prof. Hern
 counter = 0
 
 10.times do
-    user = User.create(
+    users = User.create(
         name: Faker::Name.name,
         location: locations[counter],
         description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
@@ -36,54 +36,52 @@ counter = 0
         date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 50)
     )
     counter += 1
-    reviewer = User.all - [user]
+    reviewer = User.all - [users]
     5.times do
         review = ReviewUser.create(
             rating: %w[1 2 3 4 5].sample,
             content: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
-            reviewee_id: user,
+            reviewee_id: users,
             reviewer_id: reviewer.sample
         )
     end
 end
 
 puts "Hello Users"
+users = User.all
 
-"Pule dance 1ª time", "Rock in Rio", "Visit MAAT", "New in lisbon", "", "","", "", "", "", "","", "", "", "", ""]
+event1 = Event.create!(user: users.sample, name:'Party in Urban', location:'Cais da Viscondessa, 1200-109 Lisboa', category:'Night out', date: Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Party all night long', price: 20 , max_people: 3)
+event2 = Event.create!(user: users.sample, name:'Dinner at McDonalds', location:'Av. Padre Cruz, 1600-001 Lisboa', category:'Gastronomy', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Let\s eat like an American', price: 10, max_people: 4)
+event3 = Event.create!(user: users.sample, name:'Diving in Sintra', location:'Avenida Alfredo Coelho, Colares, Sintra', category:'Sports', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Our planet it\s 71% water.', price: 50, max_people: 2)
+event4 = Event.create!(user: users.sample, name:'Trip to Porto', location:'Av. dos Aliados 54 2º, 4000-064 Porto', category:'Travel', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Join me and I will show you Porto and how to speak. first lesson P*** que pariu, cara***', price: 70, max_people: 2)
+event5 = Event.create!(user: users.sample, name:'Pole dance 1ª time', location:'R. Moreira 251, 4000-347 Porto', category:'Experiences', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Just a fancy sexy dance', price: 15, max_people:3)
+event6 = Event.create!(user: users.sample, name:'Rock in Rio', location:'Av. Dr. Arlindo Vicente 406, Lisboa', category:'Events', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'rock \'n\' roll o\'clock ', price: 80, max_people: 10)
+event7 = Event.create!(user: users.sample, name:'Visit MAAT', location:'Av. Brasília, 1300-598 Lisboa', category:'Culture', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Museum with river view', price: 5, max_people:4)
+event8 = Event.create!(user: users.sample, name:'New in Lisbon', location:'Praça do Comércio, 1100-148 Lisboa', category:'Meet', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'just show me the city pleaseeee', price:2, max_people:5)
+event9 = Event.create!(user: users.sample, name:'Climbing', location:'Edifício Lobo Rua Angola 1 Frente, 2620-036 Olival Basto', category:'Sports', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'As someone said  is natural to human beings. We walk, run and climb', price: 10, max_people:2)
+event10 = Event.create!(user: users.sample, name:'Run to Cascais', location:'Passeio de Dom Luís I, 2750-642 Cascais', category:'Sports', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Run Forest RUNNNNNNN', price: 0, max_people:4)
+event11 = Event.create!(user: users.sample, name:'Fancy Dinner', location:'Av. Marginal, 2780-267 Praia da Torre', category:'Gastronomy', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'A dinner like monaco but in Oeiras', price:40, max_people:3)
+event12 = Event.create!(user: users.sample, name:'Felling Lonely :(', location:'R. da Rosa 265, 1200-385 Lisboa', category:'Meet', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'just a boy looking for some fun ', price:15, max_people:2)
+event13 = Event.create!(user: users.sample, name:'Alive festival', location:'Passeio Marítimo de Algés, Algés', category:'Events', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'A MEGA FESTIVAL', price:100, max_people:5)
+event14 = Event.create!(user: users.sample, name:'Visit museum', location:'Alameda dos Oceanos, 1990-155 Lisboa', category:'Culture', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Culture time', price:3, max_people:3)
+event15 = Event.create!(user: users.sample, name:'Let/s go to Bairro', location:'Praça Luís de Camões, 1200-283 Lisboa', category:'Night out', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Party all night long (COVID test should be negative) and alcohol VERY positive', price:10, max_people:5)
+event16 = Event.create!(user: users.sample, name:'Paris trip', location:'Paris', category:'Travel', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Mona lisa, baguetes and molin rouge', price:90, max_people:4)
+event17 = Event.create!(user: users.sample, name:'Algarve holidays', location:'Av. Francisco Sá Carneiro, 8125-507 Quarteira', category:'Travel', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'Sun, Avencs and Camones', price:70, max_people:8)
+event18 = Event.create!(user: users.sample, name:'Sky dive', location:'Aeródromo de Évora Estr. de Viana, 7005-210 Évora', category:'Experiences', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'May the gravity be with you', price:100, max_people: 1)
+event19 = Event.create!(user: users.sample, name:'Relax Drink', location:'Av. Ribeira das Naus, Lisboa', category:'Night out', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'A good drink, a good conversation and a good sunset', price:10, max_people:5)
+event20 = Event.create!(user: users.sample, name:'Girls just wanna have fun ;)', location:'R. da Cintura do Porto de Lisboa, 1200-109 Lisboa', category:'Night out', date:Faker::Date.between(from: '2021-09-12', to: '2021-12-25'), description:'the name says it all', price:20, max_people:5)
 
-
-event1 = Event.create(name:'Party in Urban', location:'', category:'Night out', date: Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price: 20 , max_people: 3)
-event2 = Event.create(name:'Dinner at McDonalds', location:'', category:'Gastronomy', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price: 10, max_people: 4)
-event3 = Event.create(name:'Diving in Sintra', location:'', category:'Sports', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price: 50, max_people: 2)
-event4 = Event.create(name:'Trip to Porto', location:'', category:'Travel', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price: 70, max_people: 2)
-event5 = Event.create(name:'Pole dance 1ª time', location:'', category:'Experiences', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price: 15, max_people:3)
-event6 = Event.create(name:'Rock in Rio', location:'', category:'Events', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price: 80, max_people: 10)
-event7 = Event.create(name:'Visit MAAT', location:'', category:'Culture', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price: 5, max_people:4)
-event8 = Event.create(name:'New in Lisbon', location:'', category:'Meet', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:2, max_people:5)
-event9 = Event.create(name:'Climbing', location:'', category:'Sports', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price: 10, max_people:2)
-event10 = Event.create(name:'Run to Cascais', location:'', category:'Sports', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price: 0, max_people:4)
-event11 = Event.create(name:'Fancy Dinner', location:'', category:'Gastronomy', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:40, max_people:3)
-event12 = Event.create(name:'Felling Lonely :(', location:'', category:'Meet', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:15, max_people:2)
-event13 = Event.create(name:'Alive festival', location:'', category:'Events', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:100, max_people:5)
-event14 = Event.create(name:'Visit museum', location:'', category:'Culture', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:3, max_people:3)
-event15 = Event.create(name:'Let/s go to Bairro', location:'', category:'Night out', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:10, max_people:5)
-event16 = Event.create(name:'Paris trip', location:'', category:'Travel', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:90, max_people:4)
-event17 = Event.create(name:'Algarve holidays', location:'', category:'Travel', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:70, max_people:8)
-event18 = Event.create(name:'Sky dive', location:'', category:'Experiences', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:100, max_people: 1)
-event19 = Event.create(name:'Relax Drink', location:'', category:'', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:10, max_people:5)
-event20 = Event.create(name:'Girls just wanna have fun ;)', location:'', category:'Night out', date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'), description:'', price:20, max_people:5)
-
-20.times do
-    event = Event.create(
-        name: ,
-        location: Faker::Address.city,
-        category:['Night out', 'Gastronomy', 'Sports', 'Travel', 'Experiences', 'Events', 'Culture', 'Meet'].sample,
-        date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'),
-        description:Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 2),
-        price: [10, 20, 5, 2].sample,
-        max_people: [1,2,4,6,10].sample,
-        user: User.all.sample
-    )
-end
+# 20.times do
+#     event = Event.create(
+#         name: ,
+#         location: Faker::Address.city,
+#         category:['Night out', 'Gastronomy', 'Sports', 'Travel', 'Experiences', 'Events', 'Culture', 'Meet'].sample,
+#         date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'),
+#         description:Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 2),
+#         price: [10, 20, 5, 2].sample,
+#         max_people: [1,2,4,6,10].sample,
+#         user_id: User.all.sample
+#     )
+# end
 
 puts "UHUH new Events"
