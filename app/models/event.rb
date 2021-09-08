2fa0d724338389cccb600aac8b6ef268a5b16005
david luis <davidluis2020@gmail.com>
2021-09-08 12:46:02 +0100

    Update Schema and change map from user to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :booking_days
+  geocoded_by :location
 
+  after_validation :geocode, if: :will_save_change_to_location?
   validates :location, :name, :category, :date, :description, presence: true
   validates :category, inclusion: { in: ['Night out', 'Gastronomy', 'Sports', 'Travel', 'Experiences', 'Events', 'Culture', 'Meet'] }
 end
