332320192019ca0a6e7fbcdea3441a3e19767876
Mariana Kobayashi <marianadanifk@gmail.com>
2021-09-08 12:09:40 +0100

    added carousel and categories. It's missing paths and css

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require  'faker'
+require 'faker'
 
 ReviewUser.destroy_all
 Event.destroy_all
 User.destroy_all
 
-
 antonio = User.create(name: 'António Guerra', email: 'antonioguerrained@gmail.com', password: '123123', location: 'Via Futebol Clube do Porto', date_of_birth: Faker::Date.birthday(min_age: 24, max_age: 25), gender: 'male', phone_number: '+351927624961', description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
-
 david = User.create(name: 'David Luis', email: 'davidluis2020@gmail.com', password: '123123', location: 'Rua Manuel Arriaga', date_of_birth: Faker::Date.birthday(min_age: 26, max_age: 27), gender: 'male', phone_number: '+351912345678', description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
 mariana = User.create(name: 'Mariana Kobayashi', email: 'marianadanifk@gmail.com', password: '123123', location: 'Av. do Uruguai 36', date_of_birth: Faker::Date.birthday(min_age: 23, max_age: 25), gender: 'female', phone_number: '+351937826634', description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
 
 puts "Hello António, David and Mariana!"
-
 
 locations = ["Rua Condo Redondo Nº117","Rua Manuel Arriaga Nº8","R. Prof. Hernâni Cidade","Av. da Liberdade","Rua Dom João V","Via Futebol Clube do Porto","R. Dom Tomás de Almeida 61","Via Circular do Montijo","R. Pedro Álvares Cabral 42", "Av. do Uruguai 36"]
 counter = 0
@@ -38,11 +35,11 @@ counter = 0
     counter += 1
     reviewer = User.all - [user]
     5.times do
-        review = ReviewUser.create(
-            rating: %w[1 2 3 4 5].sample,
-            content: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
-            reviewee_id: user,
-            reviewer_id: reviewer.sample
+      review = ReviewUser.create(
+        rating: %w[1 2 3 4 5].sample,
+        content: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+        reviewee_id: user,
+        reviewer_id: reviewer.sample
         )
     end
 end
@@ -50,14 +47,14 @@ end
 puts "Hello Users"
 
 20.times do
-    event = Event.create(
-        name: Faker::Coffee.notes,
-        location: Faker::Address.city,
-        category:%w[Diving Surf Dinner Festival NightOut Climbing].sample,
-        date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'),
-        description:Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
-        user: User.all.sample
+  event = Event.create(
+    name: Faker::Coffee.notes,
+    location: Faker::Address.city,
+    category:%w[Diving Surf Dinner Festival NightOut Climbing].sample,
+    date:Faker::Date.between(from: '2021-09-01', to: '2021-12-25'),
+    description:Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+    user: User.all.sample
     )
 end
 
-puts "UHUH new Events"
+puts "Uhuh new events"
