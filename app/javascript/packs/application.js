e2a5a89e62accd35c9f9ed602471ff29703986b6
Mariana Kobayashi <marianadanifk@gmail.com>
2021-09-14 16:18:49 +0100

    Front-end of chatroom

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ import { initTyped } from '../plugins/init_typejs';
 import { initCarousel } from '../plugins/init_carousel';
 import { initSweetalert } from '../plugins/init_sweetalert';
 import { addEvent } from '../plugins/init_addevent';
+// import { initChatroom } from '../plugins/init_chatroom';
 // import { initChatroomCable } from '../channels/chatroom_channel';
 
 Rails.start()
@@ -40,5 +41,6 @@ document.addEventListener('turbolinks:load', () => {
   initTyped();
   initCarousel();
   initMapbox();
+  // initChatroom();
   // initChatroomCable();
 });
