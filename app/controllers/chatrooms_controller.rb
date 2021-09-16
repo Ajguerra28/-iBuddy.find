b899cd90405aa76a5dc7f3c8e4cae84dcc1ee1ab
Antonio Guerra <antonioguerrained@gmail.com>
2021-09-16 16:55:45 +0100

    a

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,7 +5,7 @@ class ChatroomsController < ApplicationController
 
   def show
     @chatroom = Chatroom.find(params[:id])
-    @event = Event.find(params[:id])
+    @event = @chatroom.event
     @message = Message.new(params[:content])
     authorize @chatroom
   end
